c55bcfcd97d639a2cb6414d3c1adb3ed14aebec5
Pu-Hsuan Wu <hwupu.cs01@g2.nctu.edu.tw>
2020-10-04 17:18:30 +0700

    Add NUXT config

---
+++ nuxt.config.js
@@ -0,0 +1,10 @@
+export default {
+  head: {
+    titleTemplate: '%s - MMD',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+    ]
+  },
+  target: 'static'
+}
