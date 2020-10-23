e47d66abad3815cdb5bf8882ea4ad25ac1e16c9b
Pu-Hsuan Wu <hwupu.cs01@g2.nctu.edu.tw>
2020-10-23 20:56:32 +0700

    New html/css layout

---
+++ components/nakirigami-form.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <section class="nkr-title nkr-form">
+      <div class="nkr-nakirikumi">百鬼組</div>
+      <div class="nkr-application">入組申請書</div>
+    </section>
+
+    <section class="nkr-form">
+      <form>
+        <ul>
+          <li>
+            <label>名稱</label>
+            <input class="input" type="text">
+          </li>
+          <li>
+            <label>所在地</label>
+            <input class="input" type="text">
+          </li>
+          <li>
+            <label>入教契機</label>
+            <textarea class="input"></textarea>
+          </li>
+          <li>
+            <label>最喜歡大小姐的哪一點？</label>
+            <textarea class="input"></textarea>
+          </li>
+          <li>
+            <label>想對大小姐說的話？</label>
+            <textarea class="input"></textarea>
+          </li>
+          <li>
+            <label>入教日期</label>
+            <input class="input" type="text">
+          </li>
+          <li>
+            <label></label>
+            <button class="button is-medium">入教</button>
+          </li>
+        </ul>
+      </form>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,49 +1,22 @@
 <template>
   <div>
-    <section class="nkr-title nkr-form">
-      <div class="nkr-nakirigami">百鬼組</div>
-      <div class="nkr-application">入組申請書</div>
-    </section>
-
-    <section class="nkr-form">
-      <form>
-        <ul>
-          <li>
-            <label>名稱</label>
-            <input class="input" type="text">
-          </li>
-          <li>
-            <label>所在地</label>
-            <input class="input" type="text">
-          </li>
-          <li>
-            <label>入教契機</label>
-            <textarea class="input"></textarea>
-          </li>
-          <li>
-            <label>最喜歡大小姐的哪一點？</label>
-            <textarea class="input"></textarea>
-          </li>
-          <li>
-            <label>想對大小姐說的話？</label>
-            <textarea class="input"></textarea>
-          </li>
-          <li>
-            <label>入教日期</label>
-            <input class="input" type="text">
-          </li>
-          <li>
-            <label></label>
-            <button class="button is-medium">入教</button>
-          </li>
-        </ul>
-      </form>
-    </section>
+    <button v-on:click="sign()">sign</button>
+    <nakirigami-form />
   </div>
 </template>
 
 <script>
 export default {
-  layout: 'nakiri2020'
+  layout: 'nakiri2020',
+  computed: {
+    counter () {
+      return this.$store.state.counter
+    }
+  },
+  methods: {
+    sign () {
+      this.$store.commit('sign')
+    }
+  }
 }
 </script>
